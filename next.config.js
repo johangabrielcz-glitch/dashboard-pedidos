9bb589fe83327f27b6ecf93a39298e77b17718a1
johangabrielcz-glitch <johangabriel.cz@gmail.com>
2025-10-22 15:01:32 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  output: 'standalone' // asegura que Vercel construya un build completo de Next.js
+}
 
 module.exports = nextConfig;
